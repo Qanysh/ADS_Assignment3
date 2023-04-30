86ff4960913c7c5bdbb4a9d529a8548144f79855
Sepa <115605390+Qanysh@users.noreply.github.com>
2023-04-30 09:37:46 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,5 +10,21 @@ public class Main {
             MyTestingClass key = new MyTestingClass(rand.nextInt(1000), "name" + i);
             table.put(key, i);
         }
+
+        // count the number of elements in each bucket
+        int[] bucketCounts = new int[table.M];
+        for (int i = 0; i < table.M; i++) {
+            MyHashTable<MyTestingClass, Integer>.HashNode<MyTestingClass, Integer> node = table.chainArray[i];
+            while (node != null) {
+                bucketCounts[i]++;
+                node = node.next;
+            }
+        }
+
+        // print the bucket counts
+        for (int i = 0; i < table.M; i++) {
+            System.out.println("Bucket " + i + ": " + bucketCounts[i]);
+        }
     }
+}
 }

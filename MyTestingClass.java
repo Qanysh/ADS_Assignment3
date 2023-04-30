6a2f9738effc8d9cdefb47d1f73856f8be103acc
Sepa <115605390+Qanysh@users.noreply.github.com>
2023-04-30 09:37:26 +0600

    Update MyTestingClass.java

--- MyTestingClass.java
+++ MyTestingClass.java
@@ -8,4 +8,23 @@ public class MyTestingClass {
         this.id = id;
         this.name = name;
     }
+
+    // Custom hashCode method that uses both id and name fields
+    @Override
+    public int hashCode() {
+        int sum = 17; // start with a prime number
+        sum = 31 * sum + id;
+        sum = 31 * sum + (name != null ? name.hashCode() : 0);
+        return sum;
+    }
+
+    // equals method for testing purposes
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        MyTestingClass other = (MyTestingClass) obj;
+        return id == other.id && (Objects.equals(name, other.name));
+    }
 }
+

9d5a660f197b482dccbe504679b4f2ea6ee95f6e
Sepa <115605390+Qanysh@users.noreply.github.com>
2023-04-30 09:33:33 +0600

    Create MyHashTable.java

---
+++ MyHashTable.java
@@ -0,0 +1,17 @@
+public class MyHashTable<K, V> {
+    static class HashNode<K, V> {
+        private final K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + " " + value + "}";
+        }
+    }
+}

b6edecea6423d108cfc6f5bcb474650e17f0d732
Sepa <115605390+Qanysh@users.noreply.github.com>
2023-04-30 09:36:28 +0600

    Update MyHashTable.java

--- MyHashTable.java
+++ MyHashTable.java
@@ -13,7 +13,13 @@ public class MyHashTable<K, V> {
         public String toString() {
             return "{" + key + " " + value + "}";
         }
-            public MyHashTable() {
+    }
+
+    public HashNode<K, V>[] chainArray;
+    private int M = 11; // default number of chains
+    private int size;
+
+    public MyHashTable() {
         chainArray = new HashNode[M];
     }
 
@@ -70,6 +76,32 @@ public class MyHashTable<K, V> {
             }
             prev = node;
             node = node.next;
+        }
+        return null;
+    }
+
+    public boolean contains(V value) {
+        for (int i = 0; i < M; i++) {
+            HashNode<K, V> node = chainArray[i];
+            while (node != null) {
+                if (node.value.equals(value)) {
+                    return true;
+                }
+                node = node.next;
+            }
+        }
+        return false;
+    }
+
+    public K getKey(V value) {
+        for (int i = 0; i < M; i++) {
+            HashNode<K, V> node = chainArray[i];
+            while (node != null) {
+                if (node.value.equals(value)) {
+                    return node.key;
+                }
+                node = node.next;
+            }
         }
         return null;
     }

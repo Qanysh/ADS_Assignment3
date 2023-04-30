daecda8f7d3149a63bacfaaca95c043fd6df9866
Sepa <115605390+Qanysh@users.noreply.github.com>
2023-04-30 09:34:14 +0600

    Create MyTestingClass.java

---
+++ MyTestingClass.java
@@ -0,0 +1,11 @@
+import java.util.Objects;
+
+public class MyTestingClass {
+    private final int id;
+    private final String name;
+
+    public MyTestingClass(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
